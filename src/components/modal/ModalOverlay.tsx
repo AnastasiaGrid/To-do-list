6eeedfb7c2d1deefb406bcb2df21eba4a3f6a897
Anastasia Gridneva <anastasia.gridneva@mail.ru>
2025-02-05 17:55:54 +0300

    Updates

--- src/components/modal/ModalOverlay.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-import './modal.scss';
-
-export function ModalOverlay({ onClick }: { onClick: () => void }): ReactElement {
-  return (
-    <div className="overlay" onClick={onClick}></div>
-  );
-}
--- src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const optionsPriority = ['high', 'medium', 'low'];
-
-export const optionsStatus = ['to do', 'in progress', 'done'];
